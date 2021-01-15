b20f85ceed83a2e53b6d533746d3329315196187
thiippal <tuomo.hiippala@iki.fi>
2021-01-15 10:31:42 +0200

    add logo

--- source/conf.py
+++ source/conf.py
@@ -49,7 +49,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = "sphinx_book_theme"
-html_title = "Applied Language Technology"
+html_title = ""
+html_logo = "../img/logo.png"
+
 html_theme_options = {
 	"use_download_button": True,
 	"use_repository_button": False,
