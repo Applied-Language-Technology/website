1571cdb3e60e78acee5dbcbea4a62f9e91fc8f3e
thiippal <tuomo.hiippala@iki.fi>
2021-11-19 21:17:46 +0200

    binder test

--- source/conf.py
+++ source/conf.py
@@ -65,8 +65,8 @@ html_theme_options = {
 	"use_download_button": True,
 	"use_repository_button": True,
 	"use_edit_page_button": False,
-	"repository_url": "https://github.com/Applied-Language-Technology/website/",
-	"path_to_docs": "source/",
+	"repository_url": "https://github.com/Applied-Language-Technology/website",
+	"path_to_docs": "source",
 	"repository_branch": "binder",
 	"launch_buttons": {"binderhub_url": "https://mybinder.org",
 					    "notebook_interface": "jupyterlab"}
